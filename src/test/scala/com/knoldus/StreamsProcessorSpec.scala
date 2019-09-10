7cd575cc01c3d5221c2e9a9a0c61d2c6f5294cc3
shubhamdangare <shubham17495@gmail.com>
2019-09-10 17:08:22 +0530

    modified IT for getMessageCountForGivenInterval

--- src/test/scala/com/knoldus/StreamsProcessorSpec.scala
+++ src/test/scala/com/knoldus/StreamsProcessorSpec.scala
@@ -16,32 +16,8 @@ class StreamsProcessorSpec extends FlatSpec with BeforeAndAfter {
   }
 
   "Spark Kafka TimeStamp Integration Testing" should "Get the count in between interval" in {
-    import org.apache.spark.sql.functions._
 
-    val dfReadStream = spark
-      .readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "test2")
-      .option("startingOffsets", "earliest")
-      .option("enable.auto.commit", "false")
-      .load()
-      .selectExpr("timestamp")
-      .filter(col("timestamp").geq(lit("2019-7-31 04:03:34")) && col("timestamp").leq(lit("2018-8-28 04:03:34")))
-      .toDF("TimeStampInBetweenCalculate")
-
-    dfReadStream.createOrReplaceTempView("TimeStampInBetweenCalculated")
-    val wordCountsDataFrame =
-      spark.sql("select count(*) as total from TimeStampInBetweenCalculated HAVING COUNT(*)>1")
-
-    val dfWriteStream = wordCountsDataFrame
-      .withColumn("SparkBatchCompletedAt", lit(current_timestamp()))
-      .writeStream
-      .format("console")
-      .outputMode("complete")
-      .start()
-
-    dfWriteStream.stop()
+    StreamsProcessor.getMessageCountForGivenInterval(spark, Array("localhost:9092", "test2", "2019-7-31 04:03:34", "2018-8-28 04:03:34"), false)
 
   }
 
